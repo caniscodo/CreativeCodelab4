0b31f5f8a9dd22c27e64d3007757ec4124180dc2
Hannah Schick <caniscodo@outlook.com>
2023-06-13 22:54:48 +0200

    PlanetSpawner

--- CreativeCodeLab4_Game/Assets/Scripts/PlanetSpawner.cs
+++ CreativeCodeLab4_Game/Assets/Scripts/PlanetSpawner.cs
@@ -4,8 +4,10 @@ public class PlanetSpawner : MonoBehaviour
 {
     public GameObject objectPrefab;
     public int numberOfObjects;
-    public int objectLimit;
+    /*public int objectLimit;*/
     public Vector3 spawnArea;
+    public string[] exclusionTags;
+    public Material[] availableMaterials;
 
     private int spawnedObjects = 0;
 
@@ -13,17 +15,59 @@ public class PlanetSpawner : MonoBehaviour
     {
         for (int i = 0; i < numberOfObjects; i++)
         {
-            if (spawnedObjects >= objectLimit)
-                break;
+            Vector3 spawnPosition = GenerateRandomSpawnPosition();
 
-            Vector3 spawnPosition = transform.position + new Vector3(
-                Random.Range(-spawnArea.x, spawnArea.x),
-                Random.Range(-spawnArea.y, spawnArea.y),
-                Random.Range(-spawnArea.z, spawnArea.z)
-            );
+            // Check if the spawn position is within any exclusion zone
+            if (IsPositionInExclusionZone(spawnPosition))
+                continue;
 
-            Instantiate(objectPrefab, spawnPosition, Quaternion.identity);
+            Material randomMaterial = GetRandomMaterial();
+            GameObject spawnedObject = Instantiate(objectPrefab, spawnPosition, Quaternion.identity);
+            ApplyMaterialToGameObject(spawnedObject, randomMaterial);
+
             spawnedObjects++;
+
+            if (spawnedObjects >= numberOfObjects)
+                break;
+        }
+    }
+
+
+    private Vector3 GenerateRandomSpawnPosition()
+    {
+        return transform.position + new Vector3(
+            Random.Range(-spawnArea.x, spawnArea.x),
+            Random.Range(-spawnArea.y, spawnArea.y),
+            Random.Range(-spawnArea.z, spawnArea.z)
+        );
+    }
+
+    private bool IsPositionInExclusionZone(Vector3 position)
+    {
+        Collider[] colliders = Physics.OverlapSphere(position, 0.1f);
+        foreach (Collider collider in colliders)
+        {
+            foreach (string tag in exclusionTags)
+            {
+                if (collider.CompareTag(tag))
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    private Material GetRandomMaterial()
+    {
+        int randomIndex = Random.Range(0, availableMaterials.Length);
+        return availableMaterials[randomIndex];
+    }
+
+    private void ApplyMaterialToGameObject(GameObject gameObject, Material material)
+    {
+        Renderer[] renderers = gameObject.GetComponentsInChildren<Renderer>();
+        foreach (Renderer renderer in renderers)
+        {
+            renderer.material = material;
         }
     }
 }
