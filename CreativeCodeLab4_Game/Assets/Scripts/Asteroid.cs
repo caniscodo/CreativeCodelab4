2fcd735a86631f11f72d43bae654d8b3f77d9d33
Hannah Schick <caniscodo@outlook.com>
2023-06-20 23:16:52 +0200

    AsteroidmechanicsdonebutEffect

---
+++ CreativeCodeLab4_Game/Assets/Scripts/Asteroid.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            GlobalData.instance.decreaseHealth(2);
+            Destroy(this.gameObject);
+        }
+    }
+}
